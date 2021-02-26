d31685275a108f704da45ee7d0b7c42a7b805dfa
MalinowyKulebiaczek <50968224+MalinowyKulebiaczek@users.noreply.github.com>
2021-02-26 11:50:26 +0100

    added display and win/lose functions

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,
           'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9,
           'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
+playing = True  #variable to control main while loop - it defines whether player keeps on hitting or chooses to stand
+
 
 class Card:
 
@@ -73,7 +75,7 @@ class Hand:
                 self.value -= 10
 
 
-class Chips: # in Polish: zetony
+class Chips:   # in Polish: zetony
 
     def __init__(self, total=100):
         self.total = total
@@ -86,9 +88,75 @@ class Chips: # in Polish: zetony
         self.total -= self.bet
 
 
+#  ------ Functions ------  #
+def take_bet(chips):
+    while True:
+        try:
+            bet = int(input("What is your bet? (Integer)"))
+        except ValueError:
+            print('It must  be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("You don't have enough chips! Your total of chips is ", chips.total)
+            else:
+                break
 
-playing = True
 
+def hit(deck, hand):
+    card = deck.deal()
+    hand.add_card(card)
+
+
+def hit_or_stand(deck, hand):
+    global playing
+    while True:
+        x = input("Do you want to hit or stand? [h/s]")
+
+        if x.lower() == 'h':
+            print("Player hits")
+            hit(deck, hand)
+            break
+
+        elif x.lower() == 's':
+            print("Player stands")
+            playing = False
+            break
+
+        else:
+            print("Wrong input! You must type either 'h' or 's'")
+
+
+# printing functions
+def show_some(player, dealer):
+    print("Dealer:")
+    print("<hidden card>", *dealer.cards[1:], sep='\t')
+    print(" ")
+    print("Player:")
+    print(*player.cards, sep='\t')
+
+
+def show_all(player, dealer):
+    print("Dealer:\t value:", dealer.value)
+    print(*dealer.cards, sep='\t')
+    print("Player:\t value:", player.value)
+    print(*player.cards, sep='\t')
+
+
+# win/lose functions
+def player_busts(player, dealer):
+    print("Player busts! Player value: ", player.value)
+
+
+def player_wins(player, dealer):
+    print("Player wins! Player value: ", player.value, "  Dealer value: ", dealer.value)
+
+
+def dealer_busts(player, dealer):
+    print("Dealer busts! Dealer value: ", dealer.value)
+
+
+def dealer_wins(player, dealer):
+    print("Dealer wins! Player value: ", player.value, "  Dealer value: ", dealer.value)
 '''
 # TESTY
 card1 = Card(suits[0], ranks[0])
@@ -108,7 +176,38 @@ player.add_card(deck.deal())
 player.add_card(deck.deal())
 print(player.value)
 print(player)
+deck = Deck()
+print(deck)
+deck.shuffle_deck()
+print(deck)
+player = Hand()
+player.add_card(deck.deal())
+player.add_card(deck.deal())
+print(player.value)
+print(player)
+
+print("Player cards", *player.cards, sep='\t ')
 '''
+
+'''
+deck = Deck()
+deck.shuffle_deck()
+print(deck)
+player = Hand()
+dealer = Hand()
+dealer.add_card(deck.deal())
+dealer.add_card(deck.deal())
+dealer.add_card(deck.deal())
+player.add_card(deck.deal())
+player.add_card(deck.deal())
+player.add_card(deck.deal())
+player.add_card(deck.deal())
+
+show_some(player, dealer)
+print("dealer:", dealer)
+print("player:", player)
+'''
+
 
 if __name__ == '__main__':
     pass
